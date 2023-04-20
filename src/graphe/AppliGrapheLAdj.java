07ff20c8877c30c073f3dbd40b9dffb1133d4371
paulpp78 <perigault.paul@gmail.com>
2023-04-20 21:34:55 +0200

    Ajout de Dijkstra

---
+++ src/graphe/AppliGrapheLAdj.java
@@ -0,0 +1,201 @@
+package graphe;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+public class AppliGrapheLAdj {
+
+    @Test
+    public void testAjouterSommet() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        assertTrue(g.contientSommet("A"));
+        assertTrue(g.contientSommet("B"));
+        assertTrue(g.contientSommet("C"));
+        assertFalse(g.contientSommet("D"));
+    }
+
+    @Test
+    public void testAjouterArc() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterArc("A", "B", 3);
+        g.ajouterArc("A", "C", 4);
+        assertTrue(g.contientSommet("A"));
+        assertTrue(g.contientSommet("B"));
+        assertTrue(g.contientSommet("C"));
+        assertTrue(g.getValuation("A","B") == 3);
+        assertTrue(g.contientArc("A", "B"));
+        assertTrue(g.contientArc("A", "C"));
+        assertFalse(g.contientArc("B", "A"));
+        assertFalse(g.contientArc("C", "A"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAjouterArcNegatif() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterArc("A", "B", -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAjouterArcExistant() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterArc("A", "B", 3);
+        g.ajouterArc("A", "B", 5);
+    }
+
+    @Test
+    public void testOterSommet() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        g.ajouterArc("A", "B", 3);
+        g.ajouterArc("A", "C", 4);
+        g.oterSommet("B");
+        assertFalse(g.contientSommet("B"));
+        assertFalse(g.contientArc("A", "B"));
+        assertTrue(g.contientSommet("A"));
+        assertTrue(g.contientSommet("C"));
+        assertTrue(g.contientArc("A", "C"));
+    }
+
+    @Test
+    public void testOterArc() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterArc("A", "B", 3);
+        g.ajouterArc("A", "C", 4);
+        g.oterArc("A", "B");
+        assertFalse(g.contientArc("A", "B"));
+        assertTrue(g.contientArc("A", "C"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOterArcInexistant() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterArc("A", "B", 3);
+        g.oterArc("A", "C");
+    }
+
+    @Test
+    public void testGetSommets() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        g.ajouterSommet("A");
+        List<String> sommets = g.getSommets();
+        assertEquals(3, sommets.size());
+        assertTrue(sommets.contains("A"));
+        assertTrue(sommets.contains("B"));
+        assertTrue(sommets.contains("C"));
+    }
+    @Test
+    public void testGetSucc() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        g.ajouterArc("A", "B", 2);
+        g.ajouterArc("A", "C", 5);
+
+        assertEquals(g.getSucc("A").size(), 2);
+        assertTrue(g.getSucc("A").contains("B"));
+        assertTrue(g.getSucc("A").contains("C"));
+    }
+
+    @Test
+    public void testGetValuation() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterArc("A", "B", 2);
+
+        assertEquals(g.getValuation("A", "B"), 2);
+        assertEquals(g.getValuation("B", "A"), -1);
+        assertEquals(g.getValuation("A", "C"), -1);
+    }
+
+    @Test
+    public void testContientSommet() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+
+        assertTrue(g.contientSommet("A"));
+        assertTrue(g.contientSommet("B"));
+        assertFalse(g.contientSommet("C"));
+    }
+
+    @Test
+    public void testContientArc() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        g.ajouterArc("A", "B", 2);
+
+        assertTrue(g.contientArc("A", "B"));
+        assertFalse(g.contientArc("B", "A"));
+        assertFalse(g.contientArc("A", "C"));
+    }
+
+    @Test
+    public void testToString() {
+        GrapheLAdj g = new GrapheLAdj();
+        g.ajouterSommet("A");
+        g.ajouterSommet("B");
+        g.ajouterSommet("C");
+        g.ajouterArc("A", "B", 2);
+        g.ajouterArc("A", "C", 5);
+
+        String expected = "A-B(2), A-C(5), B:, C:";
+        assertEquals(g.toString(), expected);
+    }
+/*
+    public static void main(String[] args) {
+        GrapheLAdj graphe = new GrapheLAdj();
+
+        // Ajout de sommets
+        graphe.ajouterSommet("A");
+        graphe.ajouterSommet("B");
+        graphe.ajouterSommet("C");
+        graphe.ajouterSommet("D");
+        graphe.ajouterSommet("E");
+
+        // Ajout d'arcs
+        graphe.ajouterArc("A", "B", 2);
+        graphe.ajouterArc("A", "C", 1);
+        graphe.ajouterArc("B", "D", 4);
+        graphe.ajouterArc("B", "E", 3);
+        graphe.ajouterArc("C", "B", 1);
+        graphe.ajouterArc("C", "D", 2);
+        graphe.ajouterArc("D", "E", 1);
+
+        // Vérification de l'existence de sommets et d'arcs
+        System.out.println("Contient sommet A ? " + graphe.contientSommet("A"));
+        System.out.println("Contient sommet F ? " + graphe.contientSommet("F"));
+        System.out.println("Contient arc A -> B ? " + graphe.contientArc("A", "B"));
+        System.out.println("Contient arc B -> A ? " + graphe.contientArc("B", "A"));
+
+        // Suppression de sommets et d'arcs
+        graphe.oterSommet("C");
+        graphe.oterArc("B", "E");
+
+        // Récupération de la liste des sommets et de leurs successeurs
+        System.out.println("Sommets : " + graphe.getSommets());
+        System.out.println("Successeurs de A : " + graphe.getSucc("A"));
+        System.out.println("Successeurs de D : " + graphe.getSucc("D"));
+
+        // Récupération de la valuation d'un arc
+        System.out.println("Valuation de A -> B : " + graphe.getValuation("A", "B"));
+
+        // Affichage du graphe
+        System.out.println(graphe);
+    }*/
+}
---
+++ src/graphe/GrapheLAdj.java
@@ -0,0 +1,282 @@
+package graphe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Cette classe implémente l'interface IGraphe en utilisant une liste d'adjacence pour stocker les informations du graphe.
+ */
+public class GrapheLAdj implements IGraphe {
+
+    /**
+     * La liste d'adjacence du graphe, représentée par une table de hachage qui associe chaque sommet à une liste de ses arcs adjacents.
+     */
+    private final Map<String, List<Arc>> ladj;
+
+    /**
+     * Constructeur par défaut qui crée une liste d'adjacence vide.
+     */
+    public GrapheLAdj() {
+        ladj = new HashMap<>();
+    }
+
+    /**
+     * Constructeur qui crée une liste d'adjacence à partir d'une chaîne de caractères donnée en paramètre.
+     * La chaîne doit être dans le format suivant : "(source1,destination1,poids1);(source2,destination2,poids2);..."
+     * @param str la chaîne de caractères à partir de laquelle créer le graphe
+     */
+    public GrapheLAdj(String str) {
+        this(); // appel au constructeur par défaut pour initialiser la liste d'adjacence
+        peupler(str); // appel à la méthode peupler pour ajouter les sommets et les arcs à la liste d'adjacence
+    }
+
+    /**
+     * Ajoute un sommet au graphe avec l'étiquette spécifiée.
+     * Si le sommet existe déjà, cette méthode ne fait rien.
+     * @param noeud l'étiquette du sommet à ajouter
+     */
+    @Override
+    public void ajouterSommet(String noeud) {
+        ladj.putIfAbsent(noeud, new ArrayList<>()); // si le sommet n'existe pas déjà, ajoute-le à la liste d'adjacence
+    }
+
+
+    /**
+     * Ajoute un arc au graphe entre les sommets spécifiés avec la valuation spécifiée.
+     * Si l'arc existe déjà, une exception est levée.
+     * Si la valuation est négative, une exception est levée.
+     * @param source l'étiquette du sommet source de l'arc
+     * @param destination l'étiquette du sommet destination de l'arc
+     * @param valeur la valuation de l'arc
+     * @throws IllegalArgumentException si l'arc existe déjà ou si la valuation est négative
+     */
+    @Override
+    public void ajouterArc(String source, String destination, Integer valeur) throws IllegalArgumentException {
+        // Vérifie si la valuation est négative
+        if (valeur < 0) {
+            throw new IllegalArgumentException("La valuation de l'arc ne peut pas être négative");
+        }
+
+        // Ajoute un nouvel arc à la liste d'adjacence associée au sommet source
+        List<Arc> adjacents = ladj.computeIfAbsent(source, k -> new ArrayList<>());
+
+        // Ajoute un nouvel arc à la liste d'adjacence associée au sommet destination
+        List<Arc> adjacents2 = ladj.computeIfAbsent(destination, k -> new ArrayList<>());
+
+        for (Arc a : adjacents2) {
+            if (a.getDestination().equals(destination)) {
+                throw new IllegalArgumentException("L'arc existe déjà");
+            }
+        }
+        // Vérifie si l'arc existe déjà
+        for (Arc a : adjacents) {
+            if (a.getDestination().equals(destination)) {
+                throw new IllegalArgumentException("L'arc existe déjà");
+            }
+        }
+
+        // Ajoute le nouvel arc à la liste d'arcs adjacents
+        adjacents.add(new Arc(source, destination, valeur));
+    }
+
+    /**
+     * Supprime le sommet spécifié du graphe.
+     * Si le sommet n'existe pas dans le graphe, cette méthode ne fait rien.
+     * @param noeud l'étiquette du sommet à supprimer
+     */
+    @Override
+    public void oterSommet(String noeud) {
+        // Supprime le sommet de la liste d'adjacence
+        if (ladj.remove(noeud) == null) {
+            return;
+        }
+
+        // Supprime tous les arcs adjacents au sommet à supprimer
+        for (Map.Entry<String, List<Arc>> entry : ladj.entrySet()) {
+            List<Arc> adjacents = entry.getValue();
+            for (int i = adjacents.size() - 1; i >= 0; i--) {
+                if (adjacents.get(i).getDestination().equals(noeud)) {
+                    adjacents.remove(i);
+                }
+            }
+        }
+    }
+
+    /**
+     * Supprime l'arc entre les sommets source et destination dans le graphe.
+     *
+     * @param source      le sommet source de l'arc
+     * @param destination le sommet destination de l'arc
+     *
+     * @throws IllegalArgumentException si le sommet source n'existe pas ou si l'arc à supprimer n'existe pas
+     */
+    @Override
+    public void oterArc(String source, String destination) throws IllegalArgumentException {
+        // On récupère la liste des arcs adjacents au sommet source
+        List<Arc> adjacents = ladj.get(source);
+        if (adjacents == null) {
+            // Si cette liste est nulle, cela signifie que le sommet source n'existe pas
+            throw new IllegalArgumentException("Le sommet source n'existe pas");
+        }
+        boolean arcSupprime = false;
+        for (int i = adjacents.size() - 1; i >= 0; i--) {
+            // On parcourt la liste des arcs adjacents à la recherche de celui qui mène au sommet destination
+            if (adjacents.get(i).getDestination().equals(destination)) {
+                adjacents.remove(i); // On supprime l'arc
+                arcSupprime = true;
+            }
+        }
+        if (!arcSupprime) {
+            // Si on n'a pas trouvé l'arc à supprimer, cela signifie qu'il n'existe pas
+            throw new IllegalArgumentException("L'arc à supprimer n'existe pas");
+        }
+    }
+
+    /**
+     * Retourne une liste de tous les sommets dans le graphe, triée par ordre alphabétique.
+     *
+     * @return une liste de tous les sommets dans le graphe
+     */
+    @Override
+    public List<String> getSommets() {
+        // On récupère toutes les clés du dictionnaire qui représentent les étiquettes des sommets
+        List<String> sommets = new ArrayList<>(ladj.keySet());
+        // On trie la liste des sommets par ordre alphabétique
+        Collections.sort(sommets);
+        return sommets;
+    }
+    /**
+     * Retourne une liste de tous les successeurs du sommet donné, triée par ordre alphabétique.
+     *
+     * @param sommet le sommet dont on veut connaître les successeurs
+     * @return une liste de tous les successeurs du sommet donné, triée par ordre alphabétique
+     */
+    @Override
+    public List<String> getSucc(String sommet) {
+        // Initialisation d'une nouvelle liste pour stocker les successeurs du sommet donné
+        List<String> successeurs = new ArrayList<>();
+
+        // Parcours de tous les arcs sortants du sommet donné
+        for (Arc a : ladj.get(sommet)) {
+            // Ajout du sommet destination de l'arc à la liste des successeurs
+            successeurs.add(a.getDestination());
+        }
+
+        // Tri de la liste des successeurs par ordre alphabétique
+        Collections.sort(successeurs);
+
+        // Retour de la liste des successeurs triée
+        return successeurs;
+    }
+
+
+    /**
+     * Retourne la valuation de l'arc entre les sommets source et destination dans le graphe.
+     *
+     * @param src      le sommet source de l'arc
+     * @param dest     le sommet destination de l'arc
+     * @return la valuation de l'arc entre les sommets source et destination, ou -1 si l'un des sommets n'existe pas ou si l'arc n'existe pas
+     */
+    @Override
+    public int getValuation(String src, String dest) {
+        // Vérification que le sommet source existe dans le graphe
+        if (!ladj.containsKey(src)) {
+            // Si le sommet source n'existe pas, retourne -1
+            return -1;
+        }
+
+        // Parcours de tous les arcs sortants du sommet source
+        for (Arc a : ladj.get(src)) {
+            // Vérification que le sommet destination est égal à celui recherché
+            if (a.getDestination().equals(dest)) {
+                // Si l'arc existe, retourne sa valuation
+                return a.getValuation();
+            }
+        }
+
+        // Si l'arc n'existe pas, retourne -1
+        return -1;
+    }
+
+    /**
+     * Vérifie si le graphe contient le sommet donné.
+     *
+     * @param sommet le sommet à rechercher dans le graphe
+     * @return true si le graphe contient le sommet donné, false sinon
+     */
+    @Override
+    public boolean contientSommet(String sommet) {
+        // Utilisation de la méthode containsKey de la classe HashMap pour vérifier si le sommet est présent dans la liste d'adjacence
+        return ladj.containsKey(sommet);
+    }
+
+    /**
+     * Vérifie si le graphe contient l'arc entre les sommets source et destination.
+     *
+     * @param src  le sommet source de l'arc à rechercher
+     * @param dest le sommet destination de l'arc à rechercher
+     * @return true si le graphe contient l'arc entre les sommets source et destination, false sinon
+     */
+    @Override
+    public boolean contientArc(String src, String dest) {
+        // Vérifie si le sommet source existe dans le graphe
+        if (!ladj.containsKey(src)) {
+            return false;
+        }
+        // Parcours tous les arcs partant du sommet source
+        for (Arc a : ladj.get(src)) {
+            // Si l'arc a pour destination le sommet recherché, retourne true
+            if (a.getDestination().equals(dest)) {
+                return true;
+            }
+        }
+        // Si l'arc n'a pas été trouvé, retourne false
+        return false;
+    }
+
+
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères du graphe.
+     *
+     * @return une représentation sous forme de chaîne de caractères du graphe
+     */
+    @Override
+    public String toString() {
+        return toAString();
+        /*// Crée un StringBuilder pour stocker la chaîne de caractères résultante
+        StringBuilder sb = new StringBuilder();
+        // Obtient la liste de tous les sommets
+        List<String> sommets = getSommets();
+        // Crée une liste pour stocker tous les arcs sous forme de chaînes de caractères
+        List<String> arcs = new ArrayList<>();
+        // Parcours tous les sommets
+        for (String sommet : sommets) {
+            // Obtient la liste des successeurs du sommet
+            List<String> successeurs = getSucc(sommet);
+            // Si la liste n'est pas vide, parcours tous les successeurs
+            if (successeurs != null && !successeurs.isEmpty()) {
+                for (String successeur : successeurs) {
+                    // Obtient la valuation de l'arc entre le sommet et son successeur
+                    int valuation = getValuation(sommet, successeur);
+                    // Ajoute la chaîne de caractères représentant l'arc dans la liste d'arcs
+                    arcs.add(sommet + "-" + successeur + "(" + valuation + ")");
+                }
+            }
+            // Si la liste est vide, ajoute simplement le sommet suivi d'un deux-points
+            if (successeurs == null || successeurs.isEmpty()) {
+                arcs.add(sommet + ":");
+            }
+        }
+        // Trie la liste d'arcs par ordre alphabétique
+        Collections.sort(arcs);
+        // Parcours tous les arcs et les ajoute dans le StringBuilder
+        for (String arc : arcs) {
+            sb.append(arc).append(", ");
+        }
+        // Retourne la chaîne de caractères résultante, en supprimant la dernière virgule et l'espace
+        return sb.delete(sb.length() - 2, sb.length()).toString();*/
+    }
+}
